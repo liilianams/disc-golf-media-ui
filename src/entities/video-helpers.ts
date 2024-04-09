196e584002ad6a47e66d0f0e54eec045a9fac640
Liilia N <liilia.namsing@gmail.com>
2024-04-09 11:57:15 -0700

    Fetch videos from the backend

--- src/entities/video-helpers.ts
+++ src/entities/video-helpers.ts
@@ -16,5 +16,23 @@ export const getVideos = async (): Promise<VideoResponseData[]> => {
   if (process.env.NODE_ENV === ENV.DEVELOPMENT) {
     return mockVideos.data;
   }
-  return [];
+
+  try {
+    const response = await fetch(
+      `${process.env.SERVER_BASE_URL}/videos`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          referer: process.env.CLIENT_DOMAIN!,
+        },
+        next: { revalidate: 3600 }, // Revalidate every hour
+      },
+    );
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching videos', error);
+    return [];
+  }
 }
