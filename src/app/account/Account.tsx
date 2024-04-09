ed56c5d6ea79b731953056bc40a372bfabe2691c
Liilia N <liilia.namsing@gmail.com>
2024-04-08 21:35:15 -0700

    Add account page

---
+++ src/app/account/Account.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import {
+  Alert,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Divider
+} from '@mui/material';
+import { AccountCircle, Email } from '@mui/icons-material';
+import mockUser from '@src/mocks/users.json';
+import Title from '@src/components/Title';
+
+const Account: React.FC = () => {
+  const [openModal, setOpenModal] = React.useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = React.useState<string>('');
+  // const { user, deleteAccount } = useAuth();
+  const user = mockUser.data;
+
+  const handleDeleteAccount = async () => {
+    // const response = await deleteAccount();
+    // if (response) {
+    //   setErrorMessage(response.data);
+    // }
+    setOpenModal(false);
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        minHeight: '200px',
+      }}>
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <AccountCircle/>
+        </Avatar>
+        <Title>Account</Title>
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+          <Email sx={{ color: 'lightgrey', mr: 1 }}/>
+          <Typography variant="body2">
+            {user?.email}
+          </Typography>
+        </Box>
+        <Box sx={{ height: '100%', mt: 'auto', width: '100%' }}>
+          <Divider sx={{ color: 'black', my: 1 }}/>
+          <Button color="error" size="small" onClick={() => setOpenModal(true)}>
+            Delete Account
+          </Button>
+          <Alert severity="error" sx={{ display: errorMessage !== '' ? 'flex' : 'none', alignItems: 'center', mb: 1 }}>
+            {errorMessage}
+          </Alert>
+          <Dialog open={openModal} onClose={() => setOpenModal(false)}>
+            <DialogTitle>Delete your account?</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Are you sure you want to delete your account and all associated data?
+                This action cannot be undone.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button variant="outlined" color="primary" size="small" onClick={() => setOpenModal(false)}>
+                Cancel
+              </Button>
+              <Button variant="contained" color="error" size="small" onClick={handleDeleteAccount}>
+                Confirm
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Account;
---
+++ src/app/account/page.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Account from '@src/app/account/Account';
+
+const VideosPage = async () => {
+  return (
+    <Account />
+  );
+};
+
+export default VideosPage;
