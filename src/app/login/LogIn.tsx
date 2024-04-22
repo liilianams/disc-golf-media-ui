2bcbd9c6d70d6e755b32ec5173b05b51513553a4
Liilia N <liilia.namsing@gmail.com>
2024-04-21 19:54:49 -0700

    Use useEffect when redirecting, fixes bad setState error

--- src/app/login/LogIn.tsx
+++ src/app/login/LogIn.tsx
@@ -2,6 +2,7 @@
 
 import { Divider } from '@mui/material';
 import * as React from 'react';
+import { useEffect } from 'react';
 import { LocalLogin } from './LocalLogin';
 import { InputForm } from '@src/containers/InputForm';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -12,9 +13,11 @@ const LogIn: React.FC = () => {
   const isAuthenticated = useStore((state) => state.isAuthenticated);
   const router = useRouter();
 
-  if (isAuthenticated) {
-    router.push('/videos');
-  }
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.push('/videos');
+    }
+  }, [isAuthenticated, router]);
 
   return (
     !isAuthenticated && (
